6003dafd5ccd2d2d5cedfe611fb2dd7981a9936a
v-madhu <madhukiran0472@gmail.com>
2022-02-09 22:11:01 +0530

    message

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,28 @@
+public class PrimitiveDataTypes{
+	
+public static void main(String[] args){
+	//char a="A";
+	//system.out.println("char value : "+a);
+	
+	byte b=8;
+	system.out.println("byte value : "+b);
+	
+	//short s=9;
+	//system.out.println("short value : "+s);
+	
+	//int i = 15;
+	//system.out.println("int value : "+i);
+	
+	//long l=86;
+	//system.out.println("long value : "+l);
+	
+	//float f=5034f;
+	//system.out.println("float value : "+f);
+	
+	//double d=63.00d;
+	//system.out.println("double value : "+d);
+	
+	//boolean bl=true;
+	//system.out.println("boolean value : "+bl);
+}
+}
