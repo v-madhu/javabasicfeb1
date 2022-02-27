b3e35bf01e12e01f9b0b484a93935ab1c6b2b6b0
v-madhu <madhukiran0472@gmail.com>
2022-02-27 19:55:12 +0530

    madhutwo

---
+++ PattrenPrint.java
@@ -0,0 +1,36 @@
+class uday
+{
+	public static void main(String[] args) 
+	{
+		int Percentage=56;
+		
+		if(Percentage >= 90)
+	    {
+			System.out.println("distinction");
+		  }
+		else if(Percentage >= 80)
+	    {
+			System.out.println("first Class");
+		  }
+		else if(Percentage >= 70)
+	    {
+			System.out.println("second class");
+	  	}
+		else if(Percentage >= 60)
+	    {
+			System.out.println("third class");
+		  }
+		else if(Percentage >= 40)
+	    {
+			System.out.println("four class");
+		  }
+		else if(Percentage >= 35)
+	   {
+	   System.out.println("pass");
+	   }
+	  else
+	   {
+			System.out.println("Fail");
+		 } 
+	}
+}
